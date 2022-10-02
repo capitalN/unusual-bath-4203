a9e39562ce843f428e38441ea0f3b49b9ab4b275
nikhil <angolkarnikhil@gmail.com>
2022-10-02 10:48:11 +0530

    new commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TopSection from '../components/TopSection';
+import MidSection from '../components/MidSection'
+
+export default function Home() {
+  return (
+    <div>
+      <TopSection/>
+      <MidSection/>
+    </div>
+  );
+}
